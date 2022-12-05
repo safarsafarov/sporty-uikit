fb890c2ef01f38afc3c4f77636925d80eb34619f
Safar Safarov <Safarov.safar@hotmail.com>
2022-12-05 23:15:28 +0500

    ✨

---
+++ sport-uikit/sport-uikit/Application/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  sport-uikit
+//
+//  Created by Safar Safarov on 04/12/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
---
+++ sport-uikit/sport-uikit/Application/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  sport-uikit
+//
+//  Created by Safar Safarov on 04/12/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        let tabBarController = UITabBarController()
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+    }
+
+}
+
